0095ccbd3ee276d8970fa0618fe9f879dda837e9
wiliansanello <wiliansanello@outlook.com>
2024-06-28 06:01:20 -0300

    Criação das classes mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import br.dio.desafio.dominio.Curso;
+import br.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso cursoJava = new Curso();
+        cursoJava.setTitulo("curso Java");
+        cursoJava.setDescricao("fundamentos de java");
+        cursoJava.setCargaHoraria(8);
+        System.out.println(cursoJava);
+
+        Curso cursoJs = new Curso();
+        cursoJs.setTitulo("curso Javascript");
+        cursoJs.setDescricao("fundamentos de javascript");
+        cursoJs.setCargaHoraria(4);
+        System.out.println(cursoJs);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Pilares da Orientação a Objetos");
+        mentoria.setDescricao("Entendendo o uso de POO");
+        mentoria.setData(LocalDate.now());
+        System.out.println(mentoria);
     }
 }
